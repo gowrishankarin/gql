bd0fa6a483743fa56ca0cd18f1715a9abeef3262
Gowri Shankar <gowri.turing@apollo.io>
2024-02-03 15:05:36 +0400

    GS/FE/TOPIC-4: BE, FE integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import Clients from "./components/Clients";
+import Header from "./components/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
+    <>
+    <ApolloProvider client={client}>
+    <Header />
     <div className="container">
-     <h1>Hello World</h1>
+     <Clients />
     </div>
+    
+    </ApolloProvider>
+    </>
   );
 }
 
